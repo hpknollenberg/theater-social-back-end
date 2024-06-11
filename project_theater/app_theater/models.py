f4f2fd47c869f9b604bdabf038bb35364fecbf91
hpknollenberg <hpknollenberg@gmail.com>
2024-06-11 10:53:32 -0400

    admin check resolved

--- project_theater/app_theater/models.py
+++ project_theater/app_theater/models.py
@@ -6,6 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     first_name = models.TextField()
     last_name = models.TextField()
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
--- project_theater/app_theater/serializers.py
+++ project_theater/app_theater/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['id', 'first_name', 'last_name']
+        fields = ['id', 'first_name', 'last_name', 'is_admin']
 
 
 class PostSerializer(serializers.ModelSerializer):
